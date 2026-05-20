55eb84ee3de3685b61d57311347f4565f3e6eccb
Kunal Sood <kunaalsood15@gmail.com>
2026-05-21 00:54:47 +0530

    Initial clean commit

--- grabMyShow/Frontend/js/api.js
+++ grabMyShow/Frontend/js/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://grabmyshow-backend.onrender/api";
+const BASE_URL =
+  "https://grabmyshow-backend.onrender.com/api";
 
 function getToken() {
   return localStorage.getItem("token");
